70714295a70a3a9f789a942043a5397ee5039703
Luke O'Malley <lukeomalley95@gmail.com>
2019-05-06 16:19:25 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -6,7 +6,9 @@ end
 
 def get_japanese_emoticon(english_emoticon)
   emoticons = load_library('./emoticons.yml')
-  
+  emoticons.each do |key, value|
+    puts key
+  end
 end
 
 def get_english_meaning

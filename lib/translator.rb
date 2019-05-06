3de17427b9b99bfc3aeaec66bbd5277070c2561a
Luke O'Malley <lukeomalley95@gmail.com>
2019-05-06 16:24:53 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -8,8 +8,7 @@ def get_japanese_emoticon(english_emoticon)
   emoticons = load_library('./emoticons.yml')
   emoticons.each do |key, value|
     if value.include?(english_emoticon)
-      puts key
-      puts value
+      puts value[1]
     end
   end
 end
